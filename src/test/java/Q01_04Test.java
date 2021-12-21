829c862994fe7e3df2d7197c6b3edcd0218a3471
Omer Kacar <omerkacar1@gmail.com>
2021-12-21 13:12:54 +0000

    feat(core): add Q01_04.java
    Can a string be permutable?

---
+++ src/test/java/Q01_04Test.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class Q01_04Test {
+
+    @Test
+    public void getCharNumber() {
+        String testString = "abcdz1";
+        Q01_04 q01_04 = new Q01_04(testString);
+        int actual = q01_04.getCharacterNumber('1');
+        int expected = -1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBuildHashTable() {
+        String testString = "abcdz1";
+        Q01_04 q01_04 = new Q01_04(testString);
+
+        int[] expected = {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+        int[] actual = q01_04.buildCharacterFrequencyTable();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testPalindromeTrue() {
+        String testString = "Tact Coa";
+        Q01_04 q01_04 = new Q01_04(testString);
+
+        assertTrue(q01_04.canBePalindrome());
+    }
+}
